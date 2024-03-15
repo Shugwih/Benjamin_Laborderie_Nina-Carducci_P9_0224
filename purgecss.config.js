13eeb2b1af9d34a40097c0c9559af79a0c01b1c5
Benjamin Laborderie <benjamin.laborderie@gmail.com>
2024-03-15 12:45:50 +0100

    Add Schema.org and meta social

---
+++ purgecss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+      'C:/Users/Utilisateur/Desktop/OC/Projet 9/ninacarducci.github.io/index.html',
+      'C:/Users/Utilisateur/Desktop/OC/Projet 9/ninacarducci.github.io/assets/maugallery.js',
+      'C:/Users/Utilisateur/Desktop/OC/Projet 9/ninacarducci.github.io/assets/scripts.js'
+  ],
+    css: ['C:/Users/Utilisateur/Desktop/OC/Projet 9/ninacarducci.github.io/assets/bootstrap/bootstrap.css'],
+    output: 'C:/Users/Utilisateur/Desktop/OC/Projet 9/ninacarducci.github.io/assets/Clean/bootstrap.css',
+    safelist: [
+      /modal/,
+      /dropdown/,
+      'my-4',
+      /tags-bar/,
+      /nav/,
+      /img/,
+      'fade',
+      /col/
+  ]
+  };
